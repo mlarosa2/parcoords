3ee0526f5a2ed8b8bf5346aeffb16ea0b88a0dd9
Mark LaRosa <mark.larosa2@gmail.com>
2017-01-26 09:07:41 -0500

    add multibrush feature, remove globalBrushed variable

--- d3_v4.parcoords.js
+++ d3_v4.parcoords.js
@@ -19,7 +19,58 @@ d3.rebind = function (target, source, method) {
   while (++i < n) target[method = arguments[i]] = d3_rebind(target, source, source[method]);
   return target;
 }
-var globalBrushed; //used for additional tools filtering
+
+//=======================
+//=======Polyfills=======
+//=======================
+
+if (!Array.prototype.includes) {
+  Object.defineProperty(Array.prototype, 'includes', {
+    value: function(searchElement, fromIndex) {
+
+      // 1. Let O be ? ToObject(this value).
+      if (this == null) {
+        throw new TypeError('"this" is null or not defined');
+      }
+
+      var o = Object(this);
+
+      // 2. Let len be ? ToLength(? Get(O, "length")).
+      var len = o.length >>> 0;
+
+      // 3. If len is 0, return false.
+      if (len === 0) {
+        return false;
+      }
+
+      // 4. Let n be ? ToInteger(fromIndex).
+      //    (If fromIndex is undefined, this step produces the value 0.)
+      var n = fromIndex | 0;
+
+      // 5. If n ≥ 0, then
+      //  a. Let k be n.
+      // 6. Else n < 0,
+      //  a. Let k be len + n.
+      //  b. If k < 0, let k be 0.
+      var k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
+
+      // 7. Repeat, while k < len
+      while (k < len) {
+        // a. Let elementK be the result of ? Get(O, ! ToString(k)).
+        // b. If SameValueZero(searchElement, elementK) is true, return true.
+        // c. Increase k by 1.
+        // NOTE: === provides the correct "SameValueZero" comparison needed here.
+        if (o[k] === searchElement) {
+          return true;
+        }
+        k++;
+      }
+
+      // 8. Return false
+      return false;
+    }
+  });
+}
 //============================================================================================
 
 d3.parcoords = function(config) {
@@ -1216,7 +1267,6 @@ function brushPredicate(predicate) {
 
 pc.brush = function() {
   __.brushed = pc.selected();
-  globalBrushed = __.brushed;
   render.call("render");
 }
 
